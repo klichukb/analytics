3a95a40d979ee4e0542c2701bbd42d3bfe76a2d7
Bogdan Klichuk <klichukb@gmail.com>
2016-11-10 01:23:35 +0200

    feat: a sentinel reader that enables receiving pings.

--- client.go
+++ client.go
@@ -13,7 +13,7 @@ import (
 // Flags
 var (
 	address     = flag.String("address", ":8000", "Websocket server address")
-	workerCount = flag.Int("workers", 100, "Amount of worker clients")
+	workerCount = flag.Int("workers", 1, "Amount of worker clients")
 	fatalCodes  = []int{
 		websocket.CloseGoingAway,
 		websocket.CloseMandatoryExtension,
@@ -30,19 +30,37 @@ var wsDialer = websocket.Dialer{
 	WriteBufferSize: 4096,
 }
 
+// This function runs in seprate goroutine to ensure we ever receive pings.
+// Without a reading goroutine, pings do not arrive.
+func readData(ws *websocket.Conn, closing chan struct{}) {
+	for {
+		// a watchman channel signaling that current client is not served
+		// anymore, which means that we should quit the loop.
+		select {
+		case <-closing:
+			return
+		default:
+		}
+		ws.ReadMessage()
+	}
+}
+
 func startClient(wsUrl, name string, sync chan int) {
 	ws, _, err := wsDialer.Dial(wsUrl, nil)
 	if err != nil {
 		log.Println("ERROR: ", err)
 	}
 
+	closing := make(chan struct{})
 	// close and signal about exiting
 	defer func() {
 		ws.Close()
 		sync <- 0
+		close(closing)
 	}()
 
 	message := []byte("Hello!")
+	go readData(ws, closing)
 
 	for {
 		if err := ws.WriteMessage(websocket.TextMessage, message); err != nil {
@@ -55,11 +73,11 @@ func startClient(wsUrl, name string, sync chan int) {
 
 func startSimulation(wsUrl string, workerCount int) {
 	sync := make(chan int)
-	for n := 1; n < workerCount; n++ {
+	for n := 0; n < workerCount; n++ {
 		go startClient(wsUrl, fmt.Sprintf("Client[%d]", n+1), sync)
 	}
 	// wait for all workers
-	for n := 1; n < workerCount; n++ {
+	for n := 0; n < workerCount; n++ {
 		<-sync
 	}
 }
